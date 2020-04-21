7bdec141af87171c0b71c04770f72da1987c533c
Rodrigo Gabriel <rodrigorgrb@gmail.com>
2020-04-20 22:32:57 -0300

    feat: variaveis básicas do jogo e funções básicas do game

---
+++ script.js
@@ -0,0 +1,36 @@
+let order = []
+let playerOrder = []
+let flash;
+let turn;
+let good;
+let compTurn;
+let intervalId;
+let strict = false;
+let noise = true;
+let on = false;
+let win;
+
+const turnCounter = document.querySelector("#turn");
+const topLeft = document.querySelector("#topLeft");
+const topRight = document.querySelector("#topRight");
+const bottomLeft = document.querySelector("#bottomLeft");
+const bottomRight = document.querySelector("#bottomRight");
+const strictButton = document.querySelector("#strict");
+const onButton = document.querySelector("#on");
+const startButton = document.querySelector("#start");
+
+strictButton.addEventListener('change', (event) => {
+  strict = strictButton.checked == true ? true : false
+})
+
+onButton.addEventListener('click', (event) => {
+  if (onButton.checked == true) {
+    on = true;
+    turnCounter.innerHTML = "-"
+  } else {
+    on = false;
+    turnCounter.innerHTML = ""
+    clearColor();
+    clearInterval(intervalId);
+  }
+})
